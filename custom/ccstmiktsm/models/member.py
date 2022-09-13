e35931317d803d098ec041daa77e6581556d35e6
Diki Hamdani <dikihamdani.id@gmail.com>
2022-09-13 14:56:08 +0700

    Update: change member models and views

--- custom/ccstmiktsm/models/member.py
+++ custom/ccstmiktsm/models/member.py
@@ -6,22 +6,38 @@ class Member(models.Model):
     _inherit = 'res.partner'
     _description = 'Anggota'
 
-    CHOICES = [
+    DIVISION_OPTIONS = [
         ('SE', 'Software Engineer'),
         ('PD', 'Product Design'),
         ('BA', 'Business Analyst'),
     ]
 
+    MAJOR_OPTIONS = [
+        ('s1teknik_informatika', 'S1 - Teknik Informatika'),
+        ('d3teknik_informatika', 'D3 - Teknik Informatika'),
+        ('d3tkj', 'D3 - TKJ'),
+    ]
+
     npm = fields.Char(string='NPM')
-    major = fields.Char(string='Jurusan')
-    division = fields.Selection(string='Divisi',
-                                selection=CHOICES,
-                                default='SE')
-    state = fields.Selection(string='Status',
-                            selection=[('aktif', 'Aktif'),
-                                        ('pasif', 'Pasif'),
-                                        ('keluar', 'keluar')],
-                            default='aktif',
-                            required=True)
+    major = fields.Selection(
+        string='Jurusan',
+        selection=MAJOR_OPTIONS,
+        default='s1teknik_informatika'
+    )
+    division = fields.Selection(
+        string='Divisi',
+        selection=DIVISION_OPTIONS,
+        default='SE'
+    )
+    state = fields.Selection(
+        string='Status',
+        selection=[
+            ('aktif', 'Aktif'),
+            ('pasif', 'Pasif'),
+            ('keluar', 'keluar')
+        ],
+        default='aktif',
+        required=True
+    )
     batch = fields.Char(string='Angkatan')
 
